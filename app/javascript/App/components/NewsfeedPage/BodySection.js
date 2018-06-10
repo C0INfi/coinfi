ef49cd3d8cfbe51d8bef78bb000e4fd166243d82
Voltron <cryptobotmachine@gmail.com>
2018-06-09 20:55:17 -0400

    wip implement react motion drawer

--- app/javascript/App/components/NewsfeedPage/BodySection.js
+++ app/javascript/App/components/NewsfeedPage/BodySection.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import Animate from 'react-move/Animate'
+import { easeExpOut } from 'd3-ease'
 import NewsItemBody from './NewsItemBody'
 import CoinBody from './CoinBody'
 import Tips from './Tips'
 
 const BodySection = (props) => {
+  const { activeEntity, mobileLayout, setMargin, animationProps } = props
+  return (
+    <div>
+      <Animate
+        show={activeEntity !== 'coin'}
+        start={{ translateX: ['-100%'] }}
+        enter={{
+          translateX: ['50%'],
+          timing: { duration: 700, ease: easeExpOut }
+        }}
+        leave={{
+          translateX: ['-100%'],
+          timing: { duration: 200 }
+        }}
+      >
+        {(animationProps) => <BodySectionInner {...props} {...animationProps} />}
+      </Animate>
+    </div>
+  )
+}
+
+
+const BodySectionInner = (props) => {
   const { activeEntity, mobileLayout, setMargin } = props
   if (!activeEntity) return <Tips />
   return (
